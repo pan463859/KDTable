2a856a2690375a930b334a9d7bd626f0bfe745de
潘小安 <panan521@qq.com>
2024-12-25 10:52:45 +0800

    fix: 树形单据体性能优化引出问题，排序条件变化后，表格数据不重新构建。

--- components/table/pipeline/features/treeMode.tsx
+++ components/table/pipeline/features/treeMode.tsx
@@ -60,6 +60,7 @@ export function treeMode (opts: TreeModeFeatureOptions = {}) {
     const primaryKey = pipeline.ensurePrimaryKey('treeMode')
 
     const openKeys: string[] = opts.openKeys ?? pipeline.getStateAtKey(stateKey) ?? opts.defaultOpenKeys ?? []
+    const curSort = pipeline.getStateAtKey('sort')
     const openKeySet = new Set(openKeys)
     const onChangeOpenKeys: TreeModeFeatureOptions['onChangeOpenKeys'] = (nextKeys: string[], key, action) => {
       opts.onChangeOpenKeys?.(nextKeys, key, action)
@@ -94,14 +95,14 @@ export function treeMode (opts: TreeModeFeatureOptions = {}) {
     return pipeline.mapDataSource(processDataSource).mapColumns(processColumns)
 
     function processDataSource (input: any[]) {
-      if(pipeline.isSameInputDataSource() && openKeys === pipeline.getFeatureOptions('lastOpenKeys')){
-        return pipeline.getFeatureOptions('lastTreeMode');
-      }
-      pipeline.setFeatureOptions('lastOpenKeys', pipeline.getStateAtKey(stateKey))
-      const result: any[] = []
-      dfs(input, 0)
-
-      function dfs (nodes: any[], depth: number) {
+        if(pipeline.isSameInputDataSource() && openKeys === pipeline.getFeatureOptions('lastOpenKeys') && curSort === pipeline.getFeatureOptions('lastSort')){
+          return pipeline.getFeatureOptions('lastTreeMode');
+        }
+        pipeline.setFeatureOptions('lastOpenKeys', openKeys)
+        pipeline.setFeatureOptions('lastSort', curSort)
+        const result: any[] = []
+        dfs(input, 0)
+      function dfs (nodes: any[], depth: number, parentNode = null) {
         if (nodes == null) {
           return
         }
